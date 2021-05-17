cde974a607fc80da9b909145dda112f9eca0bbf1
wijdench <chebbi.wijden@gmail.com>
2021-05-17 11:17:43 -0400

    'cleanup'

--- InsuranceContracting/ClientApp/src/app/models/Immutable.ts
+++
@@ -1,7 +0,0 @@
-abstract class Immutable<T> {
-    update(updates : Partial<T>): this {
-        return Object.assign(Object.create(Object.getPrototypeOf(this)), { ...this, ...updates});
-    }
-}
-
-export default Immutable;
